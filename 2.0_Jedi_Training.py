5428e63bc25257d730f8ba41c99d42f6e8dee14b
lnknisley <andthepandabomb@gmail.com>
2024-09-16 14:06:08 -0500

    Solved typos

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,25 +1,25 @@
 '''
-2.0 Jedi Training (20pts)  Name:________________
+2.0 Jedi Training (20pts)  Name: Logan Knisley
 
 UPLOAD YOUR CH 2 PDF LOG! (6pts)
 
 1.) How do you enter a single line comment in a program? Give an example. (1pt)
-
+You use a hashtag (Ex: x = 1 #sets layer)
 
 2.) If a=2 and b=5, predict all of the following below and record your predictions. (9pts)
 Then in the Python Console window, enter a=2 and b=5 and then all of the following and record the actual output.
 If the output is an error record the error and try to determine what the error is!
 Comment about any of your predictions that didn't match the actual output
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = 2.5
+b//a                = 2
+b**a                = 25
+b%a                 = 1
+a+B                 = error, undefined
+type(42)            = class 'int' (42)
+type(42.0)          = class 'float' 42.0 (42.0)
+type("C3PO")        = class 'string' (C3PO)
+type(True)          = class 'bool' (True)
 
 
 3.) Predict what would be the final output of (a) and type(a) if you enter the following 5 lines        (2pts)
@@ -31,22 +31,22 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             = 7.0 (15)
+type(a)       = class 'float'
 
 
 4.) What is the mistake in the following code? Comment it and fix it!  (1pt)
 
 x,y = (4,5)
-a = 3(x + y)
+a = 3 * (x + y)
 a
-
+# The mistake is applying math syntax to programming syntax
 
 5.) What is the mistake in the following code so it will calculate the average? Comment it and fix it!  (1pt)
 
 x,y,z =(3,4,5)
-ave = x+y+z/3
+ave = (x+y+z)/3
 ave
-
+# The mistake is fundamentally misunderstanding the order of operations
 
 '''
