e229cbfc06b8e13fd16208037c03531f1ea2fac1
lnknisley <andthepandabomb@gmail.com>
2024-09-17 14:12:09 -0500

    Solved typos

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,9 +1,7 @@
 '''
 2.0 Jedi Training (20pts)  Name: Logan Knisley
 
-UPLOAD YOUR CH 2 PDF LOG! (6pts)
-
-1.) How do you enter a single line comment in a program? Give an example. (1pt)
+1.) How do you enter a single line comment in a program? Give an example. (2pt)
 You use a hashtag in order to have a single line comment (Ex: self.world_pull = 9.80 #this sets the global gravity)
 
 2.) If a=2 and b=5, predict all of the following below and record your predictions. (9pts)
@@ -18,11 +16,11 @@ b%a                 = 1
 a+B                 = error, undefined
 type(42)            = class 'int' (42)
 type(42.0)          = class 'float' 42.0 (42.0)
-type("C3PO")        = class 'string' (C3PO)
+type("C3PO")        = class 'str' (C3PO)
 type(True)          = class 'bool' (True)
 
 
-3.) Predict what would be the final output of (a) and type(a) if you enter the following 5 lines        (2pts)
+3.) Predict what would be the final output of (a) and type(a) if you enter the following 5 lines        (3pts)
 into the Python Console Window? Then actually do it and record the output and comment on differences
 between your predictions and the output.
 
@@ -35,14 +33,14 @@ a             = 15.0
 type(a)       = class 'float'
 
 
-4.) What is the mistake in the following code? Comment it and fix it!  (1pt)
+4.) What is the mistake in the following code? Comment it and fix it!  (3pt)
 
 x,y = (4,5)
 a = 3 * (x + y)
 a
 # The mistake is applying math syntax to programming syntax, assuming that a number before parentheses multiplies it. This can be fixed by adding the multiplication operator.
 
-5.) What is the mistake in the following code so it will calculate the average? Comment it and fix it!  (1pt)
+5.) What is the mistake in the following code so it will calculate the average? Comment it and fix it!  (3pt)
 
 x,y,z =(3,4,5)
 ave = (x+y+z)/3
